12eb3ec739475470eb7f94675e18b734a9460cb0
Rithvick Kandakatla <rithvickreddy.k@gmail.com>
2025-01-08 11:43:53 +0530

    Scanner Rounding - Round Number to Nearest Multiple of 5

---
+++ Demo019.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Demo019 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        int roundedNumber = (int) (Math.round((double) number / 5) * 5);
+
+        System.out.println("The number rounded to the nearest multiple of 5 is: " + roundedNumber);
+
+        scanner.close();
+    }
+}
