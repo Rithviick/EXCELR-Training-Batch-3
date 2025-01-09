22b7a24f7d523ff3d1c41553c6c58a47fa82e2f3
Rithvick Kandakatla <rithvickreddy.k@gmail.com>
2025-01-09 13:12:03 +0530

    Fibonacci Sequence - Generate Series of N Terms

---
+++ Demo019.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Demo019 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of terms: ");
+        int n = scanner.nextInt();
+
+        getFibonacciSeries(n);
+
+        scanner.close();
+    }
+
+    public static void getFibonacciSeries(int n) {
+        if (n <= 0) {
+            System.out.println(0);
+            return;
+        }
+
+        int a = 0, b = 1;
+        System.out.print(a);
+
+        for (int i = 1; i < n; i++) {
+            System.out.print("," + b);
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+
+        System.out.println();
+    }
+}
