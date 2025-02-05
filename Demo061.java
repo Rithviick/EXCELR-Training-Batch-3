730c6045730fb063a34dec57cba233fef2251cd6
Rithvick Kandakatla <rithvickreddy.k@gmail.com>
2025-02-05 12:38:29 +0530

    Finding and Displaying the First 10 Prime Numbers

---
+++ Demo061.java
@@ -0,0 +1,27 @@
+public class Demo061 {
+    public static void main(String[] args) {
+        int count = 0;
+        int num = 2;
+        System.out.println("First 10 Prime Numbers: ");
+
+        while(count<=10){
+            if(isPrime(num)){
+                System.out.print(num + " ");
+                count++;
+
+            }
+            num++;
+        }
+    }
+    public static boolean isPrime(int n){
+        if (n < 2){
+            return false;
+        }
+        for ( int i = 2; i * i<=n; i ++){
+            if ( n % i ==0){
+                return false;
+            }
+        }
+        return true;
+    }  
+}
