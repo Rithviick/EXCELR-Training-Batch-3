2c447d307fd6367f0b7bdf3e63b37a89b6d9e379
rithviick <rithvickreddy.k@gmail.com>
2025-02-10 10:55:21 +0530

    Area Of Triangle

--- Demo013.java
+++ Demo013.java
@@ -10,6 +10,6 @@ public class Demo013 {
     public static void main(String[] args) {
         int s1 = 5, s2 = 6, s3 = 7;        
         TriangleAreaDemo1 obj = new TriangleAreaDemo1();
-        obj.getTriangleArea(s1, s2, s3); // No return, just prints
+        obj.getTriangleArea(s1, s2, s3);
     }
 }
