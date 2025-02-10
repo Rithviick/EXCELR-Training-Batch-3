ccd31def182e9fb0e13457fa879db9e6686107cc
rithviick <rithvickreddy.k@gmail.com>
2025-02-10 12:27:01 +0530

    Area Of Circle

--- Demo012.java
+++ Demo012.java
@@ -1,15 +1,11 @@
 class CircleAreaDemo1 {
-
-	void Area() {
-		
-		int r = 5;
-		
-		double ca = Math.PI* Math.pow(r, 2);
-		
+	void Area() {		
+		int r = 5;		
+		double ca = Math.PI* Math.pow(r, 2);		
 		System.out.println("The circle area is : " + ca);
 	}
-
 }
+
 public class Demo012 {
     public static void main(String[] args) {
         CircleAreaDemo1 obj = new CircleAreaDemo1();
