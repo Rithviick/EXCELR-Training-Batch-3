8cc0ab08c584a278e819ad4aa7a14fcc2c5d9d45
rithviick <rithvickreddy.k@gmail.com>
2025-02-12 12:37:26 +0530

    Print Leader Elements of an Array

---
+++ Demo081.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Demo081 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        
+        int[] arr = new int[n];
+        
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        scanner.close();
+        
+        System.out.println("Leader elements:");
+        for (int i = 0; i < n; i++) {
+            boolean isLeader = true;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] < arr[j]) {
+                    isLeader = false;
+                    break;
+                }
+            }
+            if (isLeader) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
